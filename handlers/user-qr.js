f12c82fb9fca5d0d07493e0a283ad66167f3a410
Nate <10155226+nh758@users.noreply.github.com>
2022-06-10 13:47:30 +0700

    Generate registration QR codes (#1)
    * Genereate registration QR codes
    
    * switch to async/await

---
+++ handlers/user-qr.js
@@ -0,0 +1,61 @@
+/**
+ * user-qr
+ * our Request handler.
+ */
+const InitializeRelayUser = require("../queries/InitializeRelayUser");
+const FindRelayUserByUser = require("../queries/FindRelayUserByUser");
+const ABRelay = require("../utils/ABRelay");
+const { getQRCodeData, getQRCodeBase64 } = require("../utils/ABMobile");
+const crypto = require("crypto");
+
+module.exports = {
+   /**
+    * Key: the cote message key we respond to.
+    */
+   key: "relay.user-qr",
+
+   inputValidation: {},
+
+   /**
+    * fn
+    * our Request handler.
+    * @param {obj} req
+    *        the request object sent by the
+    *        api_sails/api/controllers/relay/user-qr-page.
+    * @param {fn} cb
+    *        a node style callback(err, results) to send data when job is finished
+    */
+   fn: async function handler(req, cb) {
+      try {
+         const siteUser = req._user.uuid;
+
+         await InitializeRelayUser(req, siteUser);
+         const [relayUser] = await FindRelayUserByUser(req, siteUser);
+         const registrationToken = relayUser.registrationToken;
+         const publicKey = relayUser.rsa_public_key;
+
+         const hasher = crypto.createHash("sha256");
+         hasher.update(registrationToken);
+         const hashedToken = hasher.digest("base64");
+
+         ABRelay.post({
+            url: "/mcc/user",
+            data: {
+               user: siteUser,
+               tokenHash: hashedToken,
+               rsa: publicKey,
+            },
+            timeout: 8000,
+         }).catch((err) => {
+            req.log("Error posting registration token to MCC", err);
+         });
+
+         const deepLink = getQRCodeData(req, registrationToken);
+         const qrCode = await getQRCodeBase64(deepLink);
+
+         cb(null, qrCode);
+      } catch (e) {
+         cb(e);
+      }
+   },
+};
